c15bde3630d22fae47675952ed8e9d590be4f790
Abhinav <abhinav@amits-MacBook-Air.local>
2022-08-07 17:07:27 +0530

    [session notes: How react works Date:6th Aug-2022.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Example1 } from "./Components/Example1";
 
 function App() {
+  console.log(Example1());
+  console.log(<Example1 name='abhinav'/>);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Example1/>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// Behind the scenes of react
+// functional comp ---> react calls it directly with the props given
+// class comp ---> react created an instance of it and then calls the render method of the class
---
+++ src/Components/Example1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Example1 = () => {
+  return (
+    <>
+        <h2 className='test'>Example1</h2>
+        <section>body</section>
+    </>
+  )
+}
+
+// React.createElement('div', {className: 'test'}, reac)
